ed1249aaef4e580998e4e25d91c61bc751335f1a
artismarti <artismarti@gmail.com>
2018-11-12 18:07:44 +0000

    Adds data for Stops

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,23 @@
-Line.create(name: "Piccadilly", colour: "purple")
-Line.create(name: "Northern", colour: "black")
-Line.create(name: "Central", colour: "red")
+piccadilly = Line.create(name: "Piccadilly", colour: "purple")
+northern = Line.create(name: "Northern", colour: "black")
+central = Line.create(name: "Central", colour: "red")
 
-Station.create(name: "King's Cross")
-Station.create(name: "Leicester Square")
-Station.create(name: "Moorgate")
-Station.create(name: "Bank")
-Station.create(name: "Tottenham Court Road")
-Station.create(name: "Angel")
+kx = Station.create(name: "King's Cross")
+lsq = Station.create(name: "Leicester Square")
+moorgate = Station.create(name: "Moorgate")
+bank = Station.create(name: "Bank")
+tcr = Station.create(name: "Tottenham Court Road")
+angel = Station.create(name: "Angel")
 
+Stop.create(line_id: piccadilly.id, station_id: kx.id)
+Stop.create(line_id: piccadilly.id, station_id: lsq.id)
 
+Stop.create(line_id: northern.id, station_id: lsq.id)
+Stop.create(line_id: northern.id, station_id: kx.id)
+Stop.create(line_id: northern.id, station_id: angel.id)
+Stop.create(line_id: northern.id, station_id: bank.id)
+Stop.create(line_id: northern.id, station_id: tcr.id)
+Stop.create(line_id: northern.id, station_id: moorgate.id)
 
-
+Stop.create(line_id: central.id, station_id: bank.id)
+Stop.create(line_id: central.id, station_id: tcr.id)
