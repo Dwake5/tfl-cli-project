f74570dd45119d5a15c1c34ccad8b80ea22dc542
artismarti <artismarti@gmail.com>
2018-11-14 14:50:31 +0000

    Populates data automagically

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,27 +1,53 @@
+require_relative '../tfl.rb'
+# require 'rest-client'
+# require 'json'
+require 'set'
+
+
 Line.destroy_all
 Station.destroy_all
 Stop.destroy_all
 
-piccadilly = Line.create(name: "Piccadilly", colour: "purple")
-northern = Line.create(name: "Northern", colour: "black")
-central = Line.create(name: "Central", colour: "red")
 
-kx = Station.create(name: "King's Cross")
-lsq = Station.create(name: "Leicester Square")
-moorgate = Station.create(name: "Moorgate")
-bank = Station.create(name: "Bank")
-tcr = Station.create(name: "Tottenham Court Road")
-angel = Station.create(name: "Angel")
+piccadilly = Line.create(tfl_id: "piccadilly", name: "Piccadilly", colour: "003688")
+northern = Line.create(tfl_id: "northern", name: "Northern", colour: "000000")
+central = Line.create(tfl_id: "bakerloo", name: "Bakerloo", colour: "B36305")
+central = Line.create(tfl_id: "central", name: "Central", colour: "E32017")
+central = Line.create(tfl_id: "circle", name: "Circle", colour: "FFD300")
+central = Line.create(tfl_id: "district", name: "District", colour: "00782A")
+central = Line.create(tfl_id: "hammersmith-city", name: "Hammersmith & City", colour: "F3A9BB")
+central = Line.create(tfl_id: "jubilee", name: "Jubilee", colour: "A0A5A9")
+central = Line.create(tfl_id: "metropolitan", name: "Metropolitan", colour: "9B0056")
+central = Line.create(tfl_id: "victoria", name: "Victoria", colour: "0098D4")
+central = Line.create(tfl_id: "waterloo-city", name: "Waterloo & City", colour: "95CDBA")
 
-Stop.create(line_id: piccadilly.id, station_id: kx.id)
-Stop.create(line_id: piccadilly.id, station_id: lsq.id)
 
-Stop.create(line_id: northern.id, station_id: lsq.id)
-Stop.create(line_id: northern.id, station_id: kx.id)
-Stop.create(line_id: northern.id, station_id: angel.id)
-Stop.create(line_id: northern.id, station_id: bank.id)
-Stop.create(line_id: northern.id, station_id: tcr.id)
-Stop.create(line_id: northern.id, station_id: moorgate.id)
 
-Stop.create(line_id: central.id, station_id: bank.id)
-Stop.create(line_id: central.id, station_id: tcr.id)
+def populate_stations
+  tfl_hash = TransportForLondon::TFL
+  station_arr = []
+  tfl_hash.each do|k,v|
+    station_arr << v
+  end
+  station_arr = station_arr.flatten
+  station_set = station_arr.to_set
+
+  station_set.each do |station|
+    Station.create(name: "#{station}")
+  end
+end
+
+def populate_stops
+  tfl_hash = TransportForLondon::TFL
+  tfl_hash.each do|ln,stn|
+    l_id = Line.find_by(tfl_id: ln).id
+      stn.each do |s|
+      s_id = Station.find_by(name: s).id
+      Stop.create(station_id: s_id, line_id: l_id)
+    end
+
+  end
+end
+populate_stations
+
+populate_stops
